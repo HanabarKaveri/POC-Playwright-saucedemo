a6f4503ac25e23bd281f4ac8079ae281be9c0e3a
HanabarKaveri <168197502+HanabarKaveri@users.noreply.github.com>
2026-07-24 17:43:32 +0530

    Update playwright.config.js
    Made changes like headless to headed mode

--- playwright.config.js
+++ playwright.config.js
@@ -36,7 +36,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     baseURL: process.env.BASE_URL,
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
