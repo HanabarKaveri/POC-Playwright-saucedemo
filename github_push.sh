b1ab6e4f1511547e96ee2a95e75198f6a15d7d4c
Kaveri Basavaraj Hanabar <kaveribasavar.hanab@hcltech.com>
2026-07-26 12:03:36 +0530

    My new commit i modified sh file

--- github_push.sh
+++ github_push.sh
@@ -5,9 +5,9 @@ echo "===== Github Project Setup ====="
 REPO_URL="https://github.com/HanabarKaveri/POM-playwright-saucedemo.git"
 
 if [ ! -d ".git" ]; then
-	git init
-	git branch -M main
-	git remote add origin "$REPO_URL"
+    git init
+    git branch -M main
+    git remote add origin "$REPO_URL"
 fi
 
 git add .
@@ -17,6 +17,19 @@ read MESSAGE
 
 git commit -m "$MESSAGE"
 
+echo "Pulling latest changes from GitHub..."
+git pull origin main --rebase
+
+if [ $? -ne 0 ]; then
+    echo "Pull failed! Resolve conflicts and try again."
+    exit 1
+fi
+
+echo "Pushing changes to GitHub..."
 git push -u origin main
 
-echo "Project pushed successfully!"
+if [ $? -eq 0 ]; then
+    echo "Project pushed successfully!"
+else
+    echo "Push failed!"
+fi
